d2620c243d3e9f383c56b44da7563fa5898c27c8
LavrikDane1 <danilalavrov043@gmail.com>
2026-05-02 03:23:21 +0300

    Добавил форму профиля пользователя

--- HabitTrackerWPF/Models/Habit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HabitTracker.Models
-{
-    public class Habit
-    {
-        public string Name { get; set; }
-        public string Time { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
--- HabitTrackerWPF/UserProfile.cs
+++ HabitTrackerWPF/UserProfile.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HabitTrackerWPF.Models
+namespace HabitTrackerWPF
 {
-    internal class AppData
+    internal class UserProfile
     {
     }
 }
