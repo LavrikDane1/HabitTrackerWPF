277a22634032d76d4effcd1d06c1d01c7a05d0ac
LavrikDane1 <danilalavrov043@gmail.com>
2026-05-02 03:38:23 +0300

    улучшил

--- HabitTrackerWPF/Models/Habit.cs
+++ HabitTrackerWPF/Models/Habit.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HabitTrackerWPF.Models
+namespace HabitTrackerWPF
 {
-    internal class UserProfile
+    internal class Habit
     {
     }
 }
